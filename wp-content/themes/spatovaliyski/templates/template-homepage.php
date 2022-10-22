384f9827b54ad267e9f0c54d3841ead33081e39a
Spatovaliyski <martinspatovaliyski@gmail.com>
2022-10-22 18:35:50 +0300

    Bugfix: Fix typo in homepage's contact section

--- wp-content/themes/spatovaliyski/templates/template-homepage.php
+++ wp-content/themes/spatovaliyski/templates/template-homepage.php
@@ -250,7 +250,7 @@ get_header();
 					<h2 class="headline-title headline-scaled">Get in touch</h2>
 					
 					<div class="copy">
-						<p>I'm currently looking for new oportunities! The button below should open an email client with my Email address auto-filled.</p>
+						<p>I'm currently looking for new opportunities! The button below should open an email client with my Email address auto-filled.</p>
 					</div>
 
 					<div class="contact-group">
