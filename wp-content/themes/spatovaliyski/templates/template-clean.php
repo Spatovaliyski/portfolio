3b2910d0d20904f437c8340c554c5a5bcf90f444
Spatovaliyski <martinspatovaliyski@gmail.com>
2022-11-29 10:54:34 +0200

    Pages: Add a clean template with no content inside

---
+++ wp-content/themes/spatovaliyski/templates/template-clean.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Template Name: Clean
+*
+*/
+
+get_header();
+?>
+
+<main class="sections sections-clean">
+	<section class="section section-clean">
+		<div class="section-container">
+			<?php the_content(); ?>
+		</div>
+	</section>
+</main>
+
+<?php
+get_footer();
